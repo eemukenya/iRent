425e5626fed6c2133c7bcd46265a609da472f5fb
eemukenya <empisth@yahoo.com>
2017-03-30 22:48:12 +0300

    adding Files

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios,'9.0'
+target "iRent" do
+    use_frameworks!
+    pod 'IQKeyboardManagerSwift'
+    #pod 'SQLite.swift', '~> 0.11.2'
+end
---
+++ iRent/TenantLoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TenantLoginViewController.swift
+//  iRent
+//
+//  Created by Edwin Mukenya on 3/3/17.
+//  Copyright © 2017 Edwin Mukenya. All rights reserved.
+//
+
+import UIKit
+
+class TenantLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
